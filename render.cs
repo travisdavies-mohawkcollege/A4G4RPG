93bee038ea540e50f0e2fa2b9a44e62c6f24b070
Brayden2025 <000969117@mohawkcollege.ca>
2025-03-20 11:40:27 -0400

    add graphics

---
+++ render.cs
@@ -0,0 +1,25 @@
+﻿// Include the namespaces (code libraries) you need below.
+using System;
+using System.Numerics;
+using MohawkGame2D;
+
+// The namespace your code is in.
+namespace MohawkGame2D
+{
+    internal class render
+    {
+        Texture2D volcano = Graphics.LoadTexture("../../../assets/volcano.png");
+        Texture2D palmtree = Graphics.LoadTexture("../../../assets/palmtree.png");
+        public void volcanoRender()
+        {
+            Graphics.Draw(volcano, 150, 0);
+
+        }
+        public void palmtreeRender()
+        {
+            Graphics.Draw(palmtree, 15, 310);
+            Graphics.Draw(palmtree, 685, 310);
+        }
+        
+    }
+}
